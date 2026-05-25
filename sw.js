f91693c15901d96d0c74424cae152b8ca78cdbcd
bob loh <bobbyloh@users.noreply.github.com>
2026-05-25 00:53:40 -0700

    Add selection view and grid snap tools

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'drawvision-prototype-v1';
+const CACHE_NAME = 'drawvision-prototype-v2';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
